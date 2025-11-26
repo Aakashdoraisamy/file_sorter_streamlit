932e6b7737a88b0f38b3162f0fba5ea55daba5ac
Aakashdoraisamy <aakashdorai03@gmail.com>
2025-11-26 19:34:00 +0530

    Updated sorter.py

--- sorter.py
+++ sorter.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import logging
 from config import FILE_TYPES
 
-# Setup logging
 log_folder = Path("logs")
 log_folder.mkdir(exist_ok=True)
 logging.basicConfig(filename=log_folder / "file_log.txt", level=logging.INFO,
@@ -18,10 +17,10 @@ def sort_files(folder_path):
     for file in folder.iterdir():
         if file.is_file():
             moved = False
-            # Sort by type
+            
             for category, extensions in FILE_TYPES.items():
                 if file.suffix.lower() in extensions:
-                    # Optional: add date folder
+
                     file_date = datetime.fromtimestamp(file.stat().st_mtime)
                     dest_folder = folder / category / f"{file_date.year}-{file_date.month:02}"
                     dest_folder.mkdir(parents=True, exist_ok=True)
@@ -31,7 +30,6 @@ def sort_files(folder_path):
                     moved = True
                     break
 
-            # Files that do not match any category
             if not moved:
                 other_folder = folder / "Others"
                 other_folder.mkdir(exist_ok=True)
